0f15a968c3f5011efdf10bd9931955739cb01a4b
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-07 19:12:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   return array.join(' and ') if list.size < 3
   list[-1] = "and" + [-1]
   list.join(', ')
-  
+
  #if array.length == 2
 #  array.join (" and ")
 #elsif array.length > 2
@@ -11,4 +11,3 @@ def oxford_comma(array)
 #  array.join
 #end
 end
-
